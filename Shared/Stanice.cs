bc90ee9e937f222013cc31a51fb74f9c9c398eee
matejduckic <97177021+matejduckic@users.noreply.github.com>
2022-01-05 15:48:11 +0100

    Add files via upload

---
+++ Shared/Stanice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using pin_projekt.Client
+
+namespace pin_projekt.Stanice
+{
+    public class Stanice
+    {
+        public int staniceID { get; set; }
+
+        public string Naziv { get; set; }
+
+        public string Okretište { get; set; }
+
+    }
+}
+
